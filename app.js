71c03f554e26022cb615256da67f755aad27f6f4
Gumoro <gustavo.gumoroestudos@gmail.com>
2025-03-17 16:43:37 -0300

    Primeiro commit

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+function exibirMensagemInicial () {
+    exibirTextoNaTela ("Digite o nome dos seus amigos");
+}
+
+exibirMensagemInicial ()
+
+function adicionarNome () {
+    let nome = document.querySelector('input-name').value.trim();
+
+    if (nome == "") {
+        alert ("Digite um nome válido");
+        return;
+    }
+
+    if (listaAmigos.includes(nome)) {
+        alert("Esse nome já foi adicionado!");
+        return;    
+    }
+    
+    listaDeNomes.push(nome);
+    exibirNomesNaTela();
+
+    exibirTextoNaTela("Nome adicionado: " ${nome});
+}
+
+function exibirTextoNaTela(texto) {
+    let elemento = document.getElementById("mensagem");
+    if (elemento) {
+        elemento.textContent = texto;
+    } else {
+        console.log(texto);
+    }
+
+}
+
